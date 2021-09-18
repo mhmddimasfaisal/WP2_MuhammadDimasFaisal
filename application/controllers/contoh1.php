eb4818149cf49205154aaafe46d39f249170bfdd
mhmddimasfaisal <mhmddimasfaisal@gmail.com>
2021-09-18 23:00:11 +0700

    menampilkan biodata contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Muhammad Dimas Faisal, saya tinggal di daerah Cilendek, olahraga yang saya sukai adalah Sepakbola.";
+	}
+}
