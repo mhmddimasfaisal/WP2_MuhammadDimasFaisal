18b499fc2e0d83c0fd27b630d4ca514ee835440c
mhmddimasfaisal <mhmddimasfaisal@gmail.com>
2021-10-03 21:58:36 +0700

    upload tugas template sederhana dan dinamis

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Muhammad</li>
+		<li>Nama Belakang : Dimas Faisal</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li>Cilendek Barat Kota Bogor barat</li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Bogor</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Sepakbola</li>
+		<li>Bulutangkis</li>
+	</ul>
+</section>
