e100d840bca84ce4d46e55a6b7c002ede7ca7516
mhmddimasfaisal <mhmddimasfaisal@gmail.com>
2022-03-15 11:27:50 +0700

    Test Push

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -14,7 +14,7 @@
 				<div class="card-body">
 					<h5 class="card-title"><?= $user['nama']; ?></h5>
 					<p class="card-text"><?= $user['email']; ?></p>
-					<p class="card-text"><small class="text-muted">Jadi member sejak: <br><b><?= date('d F Y', 
+					<p class="card-text"><small class="text-muted">Menjadi member sejak : <br><b><?= date('d F Y', 
 					$user['tanggal_input']); ?></b></small></p>
 				</div>
 				<div class="btn btn-info ml-3 my-3">
